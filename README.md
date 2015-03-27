40e30d5c3f516607e512ec156cf49a446958e6d3
Jim Cummins <jcummins@fisdap.net>
2015-03-27 10:37:34 -0500

    Adds a readme, install script, dependencies

---
+++ README.md
@@ -0,0 +1,36 @@
+# ng2chat
+
+A chat app written in Angular 2
+
+## Getting started
+
+### Prerequisites
+
+* Node
+* NPM
+
+### Install dependencies
+
+The following command installs all the depenedencies for the project:
+
+**On OSX / Linux**
+
+```
+./install
+```
+
+**On windows**
+
+```
+install
+```
+
+### Running the app
+
+To run the app on port 8080 with LiveReload, run:
+
+```
+npm run serve
+```
+
+Then you can visit `http://localhost:8080` in the browser.
--- config.js
+++ config.js
@@ -4,18 +4,17 @@ System.config({
     "github:*": "jspm_packages/github/*.js",
     "*": "*.js",
     "github:angular*": "jspm_packages/github/angular/*.es6"
-  },
-  "traceurOptions": {
-    "annotations": true,
-    "types": true
   }
 });
 
 System.config({
   "map": {
+    "angular/angular": "github:angular/angular@2.0.0-alpha.15",
     "angular/zone.js": "github:angular/zone.js@0.4.1",
     "angular2": "github:angular/angular@2.0.0-alpha.15/dist/js/prod/es6/angular2",
-    "automattic/socket.io-client": "github:automattic/socket.io-client@1.3.5"
+    "automattic/socket.io-client": "github:automattic/socket.io-client@1.3.5",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
   }
 });
 
